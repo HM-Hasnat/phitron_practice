1edb9fa5557b3e2f49ee593eee6c6f7d50856f4a
Rifat Hasnat <hasnat.rifat00@gmail.com>
2022-12-29 22:17:40 +0600

    phitron_practice

---
+++ c++ programming/Module 01/pb-B.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str ;
+    int n=str.size();
+    int cnt= 1;
+    int ans = 0;
+    for (int i=0 ; i<n ; i++)
+    {
+        if(str[i] != str[i-1])
+        {
+            ans=max(ans, cnt);
+            cnt = 1 ;
+        }
+        else
+        {
+            cnt+=1;
+        }
+
+    }
+    ans =max (ans, cnt);
+    cout<<ans<<"\n";
+
+}
