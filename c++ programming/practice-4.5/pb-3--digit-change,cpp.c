b0a8c0c94fa3f8c1f97f825e56ca0e44b1449d26
Rifat Hasnat <hasnat.rifat00@gmail.com>
2023-01-13 00:43:04 +0600

    phitron_practice

---
+++ c++ programming/practice-4.5/pb-3--digit-change,cpp.c
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    string s;
+    cin<<s;
+    int i =0;
+    while( s[i]!='\0')
+    {
+        if ( s[i] = 'z')
+        {
+            cout<<'a';
+            continue;
+        }
+        if( i %2==0)
+        {
+            char c s[i]+=1;
+            cout<<c;
+        }
+        else
+            cout<<s[i]
+        i++
+    }
+    return 0;
+
+}
